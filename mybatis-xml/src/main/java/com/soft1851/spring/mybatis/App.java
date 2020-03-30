f9791f31024746f18e7740855f3b3ce5fcd46489
su <2296887348@qq.com>
2020-03-30 21:21:56 +0800

    完成mybatis-xml版模块

---
+++ mybatis-xml/src/main/java/com/soft1851/spring/mybatis/App.java
@@ -0,0 +1,16 @@
+package com.soft1851.spring.mybatis;
+
+
+/**
+ * @author su
+ * @className App
+ * @Description TODO
+ * @Date 2020/3/30 21:19
+ * @Version 1.0
+ **/
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
